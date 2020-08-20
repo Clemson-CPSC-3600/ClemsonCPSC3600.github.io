9f4da0dd6513e1f8cb95238ef7cd65d7522d193c
DESKTOP-BPGNVN7\Andrew <tauceti38@gmail.com>
2020-08-20 18:44:16 -0400

    Added videos 1-3 and 1-4

---
+++ Module1/3/scripts/config_xml.js
@@ -0,0 +1,50 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2020-08-19 09:07:55 PM" dc:source="Camtasia,20.0.5,enu" dc:title="Module 1-3" tscDM:firstFrame="Module_1-3_First_Frame.png" tscDM:originId="CECFD49B-1D4D-4DC4-ACE8-496E9D009E1F" tscDM:project="Module 1-3">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="584600"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="5FFCB26C-32B8-4547-8B04-F50111BF7E3A" tscIQ:authoredEmail="WYM9NKkQn29X9BAZNcbO5Vw2emn+rPPI370el88tFJhPc946dSAIicyxpEElBWyOzyp61ObaQf4h\
+hdUMzZ3dxpz4sDaGlj+pzOqIROb0paz6/6MaQ4PDpd5I1Ft6mpY9gZZvtcoF/oou9KPkRwH+cHOa\
+y2pPibfJGg+6tMxsecouReVhe1fao92peciE0GYhwbpvYoF/E/Up0+Mw/p/2TNPBshx9CBffrXE5\
+wwX60iHXVM7OkOMeEbLJrXxOgk1VPfpmxF+X1QuzqIGzeecqT9+tjYA2QL3SmYhy+0MWNq9Kw1kp\
+Hg06t5/rY5JWuUFwOk5wIaDAanTG9rRDaZQovA==" tscIQ:requireUserId="1" tscIQ:locale="en-US" tscIQ:reportMethod="API" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="0" tscIQ:quizHash="2f4166cb344262bea157a66eac1ed17c">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="516433" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 4"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="SHORT" tscIQ:id="0"><tscIQ:question>What are the five layers of the Internet\'s software stack? What purpose does each layer serve?</tscIQ:question><tscIQ:correctAnswer/><tscIQ:answerArray/></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="Previous"/><rdf:li xmpDM:name="txtNext" xmpDM:value="Next"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Take Quiz Now"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="Submit Answers"/><rdf:li xmpDM:name="txtReview" xmpDM:value="Replay Last Section"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="View Answers"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="Continue"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="false"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Module 1-3.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Module_1-3_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
